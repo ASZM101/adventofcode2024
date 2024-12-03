cb4f9acbb781dc9af5108ccd26ed4425bcf48bde
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2024-12-02 20:15:27 -0600

    Finished day1part2

--- Days/Day1.java
+++ Days/Day1.java
@@ -3,7 +3,8 @@ import java.io.*;
 public class Day1
 {
 	public static void main(String[] args) throws FileNotFoundException {
-	    Scanner scan = new Scanner(new File("input1.txt"));
+		// Setup
+	    Scanner scan = new Scanner(new File("input.txt"));
 		List<Integer> left = new ArrayList<Integer>();
 		List<Integer> right = new ArrayList<Integer>();
 		int distances = 0;
@@ -12,11 +13,26 @@ public class Day1
 		    right.add(scan.nextInt());
 		}
         scan.close();
+
+		// Part 1
         Collections.sort(left);
         Collections.sort(right);
 		for(int i = 0; i < left.size(); i++) {
 		    distances += Math.abs(left.get(i) - right.get(i));
 		}
-		System.out.println(distances);
+		System.out.println("Part 1: " + distances);
+
+		// Part 2
+		int score = 0;
+		for(int l : left) {
+			int frequency = 0;
+			for(int r : right) {
+				if(l == r) {
+					frequency++;
+				}
+			}
+			score += l * frequency;
+		}
+		System.out.println("Part 2: " + score);
 	}
 }
